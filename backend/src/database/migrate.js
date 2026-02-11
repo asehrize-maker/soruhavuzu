a491c91b415ebab07f2869bc5465a103277c41a1
SoruHavuzu <soru@havuzu.com>
2026-02-12 00:02:03 +0300

    Fix migration for user teams

--- backend/src/database/migrate.js
+++ backend/src/database/migrate.js
@@ -259,6 +259,7 @@ const createTables = async () => {
     await createDenemelerTables(client);
     await addGorevTipiToDenemeler(client);
     await addKullanimAlanlari(client);
+    await addKullaniciEkipleri(client);
 
   } catch (error) {
     await client.query('ROLLBACK');
